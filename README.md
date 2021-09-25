e06694267f98754676d823f5dce5371640d15ca6
lex <lex@lex.os>
2021-09-25 18:19:23 +0200

    First version

--- README.md
+++ README.md
@@ -1,2 +1,158 @@
-# passtis
-Basic password dictionary generator for french companies
+Passtis
+=======
+
+This is a password dictionary generator for France, based on the format and
+content I usually encounter during pentests in that particular country. Passtis
+supports only common formats and remains a basic tool for those who don't want
+to read the docs of complete and powerful dictionary generators.
+
+> As this should be used mostly by french-speaking people, the rest of this
+  README is in french (sorry :)).
+
+Introduction
+------------
+
+Passtis permet de générer des dictionnaires de mots de passe selon des formats
+récurrents en France, avec un contenu par défaut ou personnalisable, pour ceux
+qui ont la flemme de faire des règles pour de meilleurs outils.
+
+Pourquoi ? Après de nombreux tests d'intrusion internes, on se rend compte que :
+
+* certains formats reviennent tout le temps et c'est ceux-là qu'on veut
+  retrouver dans les dictionnaires de mots de passe qu'on génère pour faire du
+  bruteforce ;
+
+* les formats et sets de règles par défaut ou accessibles facilement sont très
+  biens, mais pas exactement comme on voudrait ;
+
+* on n'a pas le temps d'en faire des comme on voudrait sur le moment, et après
+  on oublie (jusqu'au prochain).
+
+Alors je pourrais m'embêter à faire un milliardième set de règles pour un outil,
+mais je préfère faire un milliardième outil qui fait ce que je lui demande (et
+surtout, comme je lui demande).
+
+Usage
+-----
+
+```
+$> python passtis.py -h
+usage: passtis.py [-h] [-w words] [-f file] [-p postcode (69|69100)]
+                  [-s specialchars] [-m] [-d] [-y] [-xc]
+
+French password dictionary builder
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -w word(s), --words word(s)
+                        Comma-separated word(s) to use
+  -f file, --file file  File containing a list of words
+  -p postcode (69|69100), --postcode postcode (69|69100)
+                        Postcode to use (all other are moved)
+  -s specialchars, --special specialchars
+                        Special chars to use (can be empty string)
+  -m, --month           Use months as base (in french)
+  -d, --date            Use dates as suffix with format ddmmyy
+  -y, --year            Use years as suffix with format yyyy
+  -x, --nocombo        Do not build combined words with short words
+```
+
+### TL;DR
+
+Pour utiliser Passtis de manière efficace:
+
+1. Lors d'un test dans l'entreprise "superlama" située à Aubenas (07200), sur le
+hash de l'administrateur système dont le fils s'appelle Killian et est né à
+Saint-Etienne (42000):
+
+```
+python passtis.py -w "superlama,killian" -p07200,42000
+```
+
+2. Lors d'un test dans l'entreprise "dupoulet" située en Loire-Atlantique (44),
+pour faire du bruteforce sur tous les comptes du domaine sans politique de
+verrouillage (sans compter le test login = mot de passe) :
+
+```
+python passtis.py -w "dupoulet" -p44 -dmy
+```
+
+### Détails
+
+Par défaut, je considère qu'une bonne grosse partie des mots de passe français
+est constitué d'une **base** (no shit) à laquelle peuvent être ajoutées un
+**suffixe** et/ou des **caractères spéciaux**. C'est sur ce principe que se base
+Passtis, qui va générer des mots de passe qui ont les formats suivants :
+
+```
+(BASE, SUFFIX), # Bernard42
+(BASE, SPECIALS), # Bernard*
+(SUFFIX, SPECIALS), # 260143!
+(BASE, SUFFIX, SPECIALS), # Bernard42!
+(BASE, SPECIALS, SUFFIX)  # Bernard@2021
+```
+
+Actuellement, les bases et les suffixes sont constitués d'un certain nombre de
+types d'objets, activables ou modifiables avec les arguments :
+
+```
+BASE = WORDS + MONTHS
+SUFFIX = NUM + YEARS + POSTCODES + DATES
+```
+
+Chaque base sera utilisée tel quel + en version capitalisée (ex: `En1mot`)
+
+> D'autres types d'objets sont susceptibles d'être ajoutés si je pense à
+  d'autres trucs ou si on me fait des suggestions.
+
+#### Bases personnalisées
+
+`WORDS` correspond à la liste des mots que vous aurez spécifiés manuellement ou
+en donnant un fichier de mots.
+
+```
+$> python passtis.py -w en1mot,mot,en2mots,momots!
+[...]
+$> python passtis.py -f dico.txt
+[...]
+$> cat dico.txt
+en1mot
+mot
+en2mots
+momots!
+```
+
+Pour les mots inférieurs à 5 caractères, des versions "combinées" sont également
+créées et intégrer au dictionnaire final. Ce comportement est désactivable avec
+l'option `-x`.
+
+Par exemple: `sql` et `user` produiront également des sorties `usersql` et
+`sqluser`.
+
+#### Bases prédéfinies
+
+Vous pouvez utiliser des listes de mots prédéfinifies. Pour l'instant, l'option
+`-m` permet d'utiliser également les mois comme base de mot de passe (`MONTHS`).
+Ces listes peuvent être combinées avec les options `-w` et `-f`
+
+#### Suffixes
+
+Les suffixes sont les ajouts faits à cette base, entrecoupés ou non de
+caractères spéciaux. Tous les sont désactivés par défaut à part `NUM` qui
+consiste en une séquence de chiffres de 0 à 99 et de 00 à 09.
+
+#### Caractères spéciaux
+
+Finalement, il existe un set de caractères spéciaux par défaut, mais vous pouvez
+les remplacer par les votre avec l'option `-s`:
+
+```
+$> python passtis.py -f dico.txt -s "!@"
+```
+
+Améliorations possibles
+-----------------------
+
+- [ ] Pouvoir choisir le format de la date (ex: ddmmyyyy)
+- [ ] Pouvoir choisir l'intervalle de la date (par exemple, du 01012010 au 31122011)
+- [ ] Choix de la taille minimum pour les combinaisons
---
+++ passtis.py
@@ -0,0 +1,171 @@
+# Passtis
+# Very stupid password dictionary builder for french dictionaries
+#
+# Lex @ https://github.om/claire-lex/passtis - 2021
+#
+# pylint: disable=invalid-name
+
+"""
+usage: passtis.py [-h] [-w words] [-f file] [-p postcode (69|69100)]
+                  [-s specialchars] [-m] [-d] [-y] [-xc]
+
+French password dictionary builder
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -w word(s), --words word(s)
+                        Comma-separated word(s) to use
+  -f file, --file file  File containing a list of words
+  -p postcode (69|69100), --postcode postcode (69|69100)
+                        Postcode to use (all other are moved)
+  -s specialchars, --special specialchars
+                        Special chars to use (can be empty string)
+  -m, --month           Use months as base (in french)
+  -d, --date            Use dates as suffix with format ddmmyy
+  -y, --year            Use years as suffix with format yyyy
+  -x., --nocombo        Do not build combined words with short words
+"""
+
+from datetime import date, timedelta
+from argparse import ArgumentParser
+from itertools import combinations
+
+#-----------------------------------------------------------------------------#
+# Constants & config                                                          #
+#-----------------------------------------------------------------------------#
+
+# Values that may change -----------------------------------------------------#
+
+MAX_YEAR = 2022
+SPECIALS = "&#@+$%*?/!§"
+COMBO_MAX_LEN = 5 # Maximum length of words that will be combined
+WORDS = [] # Can be changed with options 'words' and 'file'
+
+# Arguments parsing ----------------------------------------------------------#
+
+OPTS_DICT = (
+    ("-w", "--words", "Comma-separated word(s) to use", WORDS, "word(s)"),
+    ("-f", "--file", "File containing a list of words", None, "file"),
+    ("-p", "--postcode", "Postcode to use (all other are moved)", None, "postcode (69|69100)"),
+    ("-s", "--special", "Special chars to use (can be empty string)", None, "specialchars"),
+    ("-m", "--month", "Use months as base (in french)", False, None),
+    ("-d", "--date", "Use dates as suffix with format ddmmyy", False, None),
+    ("-y", "--year", "Use years as suffix with format yyyy", False, None),
+    ("-x", "--nocombo", "Do not build combined words with short words", False, None)
+)
+
+def init_options() -> object:
+    """Set options using ``ArgumentParser``."""
+    args = ArgumentParser(description="French password dictionary builder")
+    # Store all other options
+    for opt in OPTS_DICT:
+        if not opt[4]: # Option takes no argument (no meta)
+            args.add_argument(opt[0], opt[1], help=opt[2],
+                              action='store_true', default=opt[3])
+        else:
+            args.add_argument(opt[0], opt[1], help=opt[2],
+                              metavar=opt[4], default=opt[3])
+    return args.parse_args()
+
+OPTIONS = init_options()
+
+# Regular stuff --------------------------------------------------------------#
+
+# 0-10 and 00-09
+NUM = [str(x) for x in list(range(0, 100))]
+NUM += [str(x).zfill(2) for x in list(range(0, 10))]
+
+if OPTIONS.special != None: # Can be empty string
+    SPECIALS = OPTIONS.special
+
+# Dates ----------------------------------------------------------------------#
+
+MONTHS = []
+if OPTIONS.month:
+    MONTHS = ["janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet",
+              "aout", "septembre", "octobre", "novembre", "decembre"]
+
+YEARS = []
+if OPTIONS.year:
+    YEARS = [str(x) for x in range(1970, MAX_YEAR + 1)]
+
+DATES = []
+if OPTIONS.date:
+    def daterange(start_date, end_date):
+        for n in range(int((end_date - start_date).days)):
+            yield start_date + timedelta(n)
+    DATES = [x.strftime("%d%m%y") for x in daterange(date(1970, 1, 1), date(MAX_YEAR, 12, 31))]
+
+# Post codes -----------------------------------------------------------------#
+
+POSTCODES = []
+if OPTIONS.postcode:
+    # French post/zip code using INSEE's list
+    POSTCODES_FILE = "postcode_insee_20210923.csv"
+    with open(POSTCODES_FILE, 'r', encoding='ISO-8859-1') as fd:
+        postcodes = [x.strip().split(";")[1] for x in fd]
+    # We add department number (ex: 69 for 69***, 974)
+    postcodes += [str(x).zfill(2) for x in list(range(1, 96))] + [str(x) for x in range(974, 989)]
+    # Now we take only the ones we need
+    selected = [OPTIONS.postcode] if "," not in OPTIONS.postcode else OPTIONS.postcode.split(",")
+    for pc in selected:
+        POSTCODES += [x for x in postcodes if x.startswith(pc)]
+
+# Words from arguments and files ---------------------------------------------#
+
+# -w / --words
+if OPTIONS.words:
+    WORDS += [x.strip() for x in OPTIONS.words.split(",")]
+
+# -f / --file
+if OPTIONS.file:
+    try:
+        with open(OPTIONS.file, 'r', encoding='utf-8') as fd:
+            WORDS += [x.strip() for x in fd]
+    except IOError:
+        print("Error: File {0} cannot be opened.".format(OPTIONS.file))
+        exit(-1)
+        
+# Short user-supplied words (less than N chars) are used to build combined
+# words, unless option -n / --nocombo is supplied.
+# Example: words 'user' and 'ext' will combined into 'userext' and 'extuser'
+if not OPTIONS.nocombo:
+    short_words = [x for x in WORDS if len(x) < COMBO_MAX_LEN]
+    combos = [x for x in combinations(short_words, 2)]
+    WORDS += ["".join(x) for x in combos] # Regular: [user, ext] == userext
+    WORDS += ["".join(y) for y in combos[::1]] # Reverse: extuser
+
+# Remove duplicates
+WORDS = list(set(WORDS))
+
+# Final format ---------------------------------------------------------------#
+
+BASE = WORDS + MONTHS
+SUFFIX = NUM + YEARS + POSTCODES + DATES
+FORMATS = (
+    (BASE, SUFFIX), # Thierry42
+    (BASE, SPECIALS), # Bernard*
+    # (SUFFIX, SPECIALS), # 260143! - Weird result but uncomment if you need it
+    (BASE, SUFFIX, SPECIALS), # Bernard42!
+    (BASE, SPECIALS, SUFFIX)  # Thierry@2021
+)
+
+#-----------------------------------------------------------------------------#
+# Run                                                                         #
+#-----------------------------------------------------------------------------#
+
+# Base only
+for base in BASE:
+    print(base)
+
+# Iterate over formats (very dirty code, I expect to be more inspired later)
+for pformat in FORMATS:
+    for item1 in pformat[0]:
+        for item2 in pformat[1]:
+            if len(pformat) == 2:
+                print("".join([item1, item2]))
+                print("".join([item1, item2]).capitalize())
+            elif len(pformat) == 3:
+                for item3 in pformat[2]:
+                    print("".join([item1, item2, item3]))
+                    print("".join([item1, item2, item3]).capitalize())
